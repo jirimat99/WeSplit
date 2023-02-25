b0a0cf5a02f5d84c06f5eea008051eafebca1f91
jirimatousek <jiri.matousek1999@gmail.com>
2023-02-25 10:09:18 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jiri Matousek on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
