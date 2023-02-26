8ead37d279666f6ae1d3792ad9574f7f5208157f
jirimatousek <jiri.matousek1999@gmail.com>
2023-02-26 13:49:59 +0100

    before Math

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10,15,20,25,0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CZK") )
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                            
+                        }
+                    }
+                }
+                Section {
+                    Picker("Pick Tip Percentage", selection: $tipPercentage) { ForEach(tipPercentages, id: \.self) {
+                        Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CZK"))
+                }
+            }
+            .navigationTitle("WeSplit")
         }
-        .padding()
     }
 }
 
