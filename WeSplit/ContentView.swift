fa589a16e8284197d6a6960d689dd4cb8181bdf3
jirimatousek <jiri.matousek1999@gmail.com>
2023-02-26 14:22:18 +0100

    WeSplit done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -14,8 +14,18 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+    @FocusState private var amountIsFocused: Bool
     let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        let peoplecount = Double(numberOfPeople + 2)
+        let tipselection = Double(tipPercentage)
+        let tipValue = checkAmount/100 * tipselection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peoplecount
+    return amountPerPerson
+        }
+    
     
     var body: some View {
         NavigationView {
@@ -23,6 +33,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CZK") )
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of People", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -39,10 +50,19 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 }
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CZK"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "CZK"))
                 }
+
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
